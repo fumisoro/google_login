2b5100d5f43a1bd6c6d8714ca29a828d62c21ba5
mac Fumiya <fumipumisecky@gmail.com>
2016-03-01 12:07:56 +0900

    add google login logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  class << self
+    def find_for_google_oauth2(auth)
+      user = User.where(email: auth.info.email).first
+
+      unless user
+        user = User.create(name: auth.info.name,
+                           provider: auth.provider,
+                           uid: auth.uid,
+                           email: auth.info.email,
+                           token: auth.credentials.token,
+                           password: Devise.friendly_token[0, 20])
+      end
+      user
+    end
+  end
 end
